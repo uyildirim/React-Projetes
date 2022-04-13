79fb0cd73b7f6151273c1eddb352f31a0cdddb80
uyildirim <uguryildirim1903@gmail.com>
2022-04-13 23:02:40 +0300

    Home page remove button deleted

--- src/todo/Footer.js
+++ src/todo/Footer.js
@@ -3,7 +3,6 @@ import React from 'react';
 function Footer({handleShow,...props}) {
   return (
     <div className="d-block text-right card-footer">
-      <button className="mr-2 btn btn-link btn-sm">Cancel</button>
       <button onClick={handleShow} className="btn btn-primary">Add Task</button>
     </div>
   );
