3c94ae08a9f1d9c8db569ffa675548a0c0813294
Uğur YILDIRIM <uguryildirim1903@gmail.com>
2022-05-04 18:10:25 +0300

    Header page end

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,29 +3,31 @@ import {FaSearch, FaWindowClose} from "react-icons/fa";
 import {useState} from "react";
 
 function Header() {
-  const [search, setSearch] = useState(true);
-  return (
-    <div className="header">
-      <div className="menu">
-        <ul>
-          <li>Home</li>
-          <li>Movies</li>
-          <li>TV Shows</li>
-          <li>Watchlist</li>
-        </ul>
-      </div>
+    const [search, setSearch] = useState(false);
+    return (
+        <div className="header">
+            <div className="menu">
+                <ul>
+                    <li>Home</li>
+                    <li>Movies</li>
+                    <li>TV Shows</li>
+                    <li>Watchlist</li>
+                </ul>
+            </div>
+            <div className="search-avatar">
+                <button className="" onClick={() => setSearch(true)}><FaSearch/></button>
+                <div className={`search-box ${search ? "show" : ""}`}>
+                    <input type="text" className="search-txt"/>
+                    <button onClick={() => setSearch(false)}><FaWindowClose/></button>
+                </div>
 
-      <div className="search-box">
-        <input type="text" className="search-txt"/>
-        <button><FaWindowClose/></button>
-      </div>
-
-      <div className="avatar flex items-center gap-2">
-        <span>Leslie Alexander</span>
-        <img className="h-16 rounded-full" src="https://picsum.photos/200/200"/>
-      </div>
-    </div>
-  );
+                <div className="avatar">
+                    <span>Leslie Alexander</span>
+                    <img className="avatar-image" src="https://picsum.photos/200/200"/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Header;
