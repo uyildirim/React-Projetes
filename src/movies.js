9d9b6b59526dc0adabec57d34871bcb83a11effb
Uğur YILDIRIM <uguryildirim1903@gmail.com>
2022-05-05 01:14:24 +0300

    Movie list update

--- src/movies.js
+++ src/movies.js
@@ -86,26 +86,5 @@ const movies = [
     "popularity": 6182.987,
     "media_type": "movie"
   },
-  {
-    "original_language": "en",
-    "original_title": "The Batman",
-    "poster_path": "/74xTEgt7R36Fpooo50r9T25onhq.jpg",
-    "video": false,
-    "vote_average": 7.9,
-    "overview": "In his second year of fighting crime, Batman uncovers corruption in Gotham City that connects to his own family while facing a serial killer known as the Riddler.",
-    "release_date": "2022-03-01",
-    "vote_count": 3078,
-    "adult": false,
-    "backdrop_path": "/5P8SmMzSNYikXpxil6BYzJ16611.jpg",
-    "title": "The Batman",
-    "genre_ids": [
-      80,
-      9648,
-      53
-    ],
-    "id": 414906,
-    "popularity": 4632.662,
-    "media_type": "movie"
-  },
 ]
 export default movies
